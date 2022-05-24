99a8a9ecb6dad1e3cdcfcbeba55ca351324d059c
rutiksimform <rutik.p@simformsolutions.com>
2022-05-24 17:49:07 +0530

    changes for showing the images in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # railsmoviedemo::Application.configure do
-  #   config.assets.compile = true
-  # end
+  railsmoviedemo::Application.configure do
+    config.assets.compile = true
+  end
 end
